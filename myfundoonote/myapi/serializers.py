79508b9b176641b4ccd823d12bd7bdaf9bbca822
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-16 10:23:55 +0530

    [Ibraheem Khaleel] Deleted: Unnecessary file have been removed

--- myfundoonote/myapi/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import User
-
-class AccountDetailSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id','first_name', 'last_name' , 'email' , 'password' , 'date_joined')
