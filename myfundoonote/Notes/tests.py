f255f2246ea88a455f8357c3461ec6b38705c054
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-20 21:41:25 +0530

    [Ibraheem Khaleel]Refactored: code for testing note CRUD operations

--- myfundoonote/Notes/tests.py
+++ myfundoonote/Notes/tests.py
@@ -1,18 +1,25 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
+from django.urls import  reverse
+from . import urls
+
 
 
 class Data(APITestCase):
     def setUp(self):
-        self.register_url = 'http://127.0.0.1:8000/register/'
-        self.note_get_post_url = 'http://127.0.0.1:8000/note/'
-        self.note_put_delete_url = 'http://127.0.0.1:8000/note-update/1/'
+        self.register_url = reverse('register')
+        self.note_get_post_url = reverse('note_create_retrieve')
+        self.note_put_delete_url = reverse('note_update_delete', args=[1])
 
-        self.valid_note_data = {'user': [ibrahee@gmail.com],
+        # self.register_url = 'http://127.0.0.1:8000/register/'
+        # self.note_get_post_url = reverse('note_create_retrieve')
+        # self.note_put_delete_url = 'http://127.0.0.1:8000/note-update/1/'
+
+        self.valid_note_data = {'user': 1,
                                 'title': "my testing note",
                                 'description': "Need a change",
                                 'labels': "Third Note",
-                                'collaborators': [ibrahee@gmail.com],
+                                'collaborators': [1],
                                 }
         self.valid_note_put_data = {'user': 1,
                                     'title': "this is not a demo note",
@@ -20,7 +27,7 @@ class Data(APITestCase):
                                     'labels': "second note",
                                     'collaborators': [1],
                                     }
-        self.invalid_note_data = {'user': 1,
+        self.invalid_note_data = {'user': "sadas@gmail.com",
                                   'description': "Need a change",
                                   'labels': "Third Note",
                                   'collaborators': [1],
@@ -36,6 +43,7 @@ class NotesTest(Data):
     def test_given_valid_note_url_for_crud(self):
         life = self.client.post(self.register_url, self.valid_registration_data, format='json')
         print(life)
+        print(self.valid_registration_data)
         yes = self.client.post(self.note_get_post_url, self.valid_note_data, format='json')
         print(yes)
 
@@ -45,10 +53,10 @@ class NotesTest(Data):
 
 
         response = self.client.put(self.note_put_delete_url, self.valid_note_put_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         response = self.client.delete(self.note_put_delete_url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
 
     def test_given_invalid_note_details_for_crud(self):
         response = self.client.post(self.note_get_post_url, self.invalid_note_data, format='json')
