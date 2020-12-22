75d3659e2590f245251a5f336d05a18883fc758d
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-22 09:28:37 +0530

    [Ibraheem Khaleel]Refactored: code for model for Label app

--- myfundoonote/Label/models.py
+++ myfundoonote/Label/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
@@ -7,9 +8,16 @@ class Label(models.Model):
     Created a model class for labelling notes
     """
     name = models.CharField(max_length = 130, null = True , unique = True)
+    deleted_at = models.DateTimeField(blank=True, null=True)
+    is_deleted = models.BooleanField(default = False)
 
     def __str__(self):
         return self.name
+
+    def soft_delete(self):
+        self.is_deleted = True
+        self.deleted_at = timezone.now()
+        self.save()    
 
        
 
