74d2eb4b02937a6b47af02ef6d481c4271ad2c51
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-22 08:05:20 +0530

    [Ibraheem Khaleel]Refactored: code for urls for Notes app

--- myfundoonote/Notes/urls.py
+++ myfundoonote/Notes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.NotesOverview.as_view()),
-    path('note/', views.Notes.as_view(), name = 'note_create_retrieve'),
-    path('note-update/<int:pk>/', views.UpdateNote.as_view(), name = 'note_update_delete')
+    path('note/', views.NotesView.as_view(), name = 'note_create_retrieve'),
+    path('note-update/<int:pk>/', views.NoteView.as_view(), name = 'note_update_delete'),
     ]
