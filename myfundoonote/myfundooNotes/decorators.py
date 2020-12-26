835ac4e996aa8885ba8075f8c3b376553d1fbdb7
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-26 07:44:01 +0530

    [Ibraheem Khaleel]Refactored: code for manipulating token recieved from authorization header.

--- myfundoonote/myfundooNotes/decorators.py
+++ myfundoonote/myfundooNotes/decorators.py
@@ -1,6 +1,7 @@
 import json
+import jwt
 
-from django.contrib.auth.models import User
+from .models import User
 from django.http import HttpResponse
 from django.shortcuts import redirect
 
@@ -8,19 +9,19 @@ from rest_framework_jwt.settings import api_settings
 from rest_framework import status
 
 
-def user_login_required(view_func):
-    
+def user_login_required(view_func): 
     def wrapper(request, *args, **kwargs):
-        
-        if request.session:
-            user = request.user
-            if user.is_authenticated:
-                return view_func(request, *args, **kwargs)
-            else:
-                response = {'success': False, 'message': 'User is not authenticated'}
-                return HttpResponse(json.dumps(response), status=status.HTTP_400_BAD_REQUEST)
-        else:
+        try:
+            token = request.META['HTTP_AUTHORIZATION']
+            decoded_token = jwt.decode(token, "secret", algorithms=["HS256"])
+            id = decoded_token['id']
+            user = User.objects.get(id = id)
+            if user and user.is_active:
+                request.user = user
+                return view_func(request, *args, **kwargs)   
             response = {'success': False, 'message': 'User must be logged in'}
             return HttpResponse(json.dumps(response), status=status.HTTP_400_BAD_REQUEST)
-
+        except Exception:
+            response = {'success': False, 'message': 'please provide a valid token'}
+            return HttpResponse(json.dumps(response), status=status.HTTP_400_BAD_REQUEST)       
     return wrapper
