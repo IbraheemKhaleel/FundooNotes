d54ba725667bed3af9732abdda917385d306c382
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-26 07:50:39 +0530

    [Ibraheem Khaleel]Refactored: code for manipulating token recieved from authorization header.

--- myfundoonote/myfundooNotes/decorators.py
+++ myfundoonote/myfundooNotes/decorators.py
@@ -1,22 +1,31 @@
+"""
+Author: Ibraheem Khaleel
+Created :  23 December 2020
+
+"""
+
 import json
 import jwt
 
-from .models import User
 from django.http import HttpResponse
-from django.shortcuts import redirect
 
-from rest_framework_jwt.settings import api_settings
 from rest_framework import status
+
+from .models import User
 
 
 def user_login_required(view_func): 
+    """
+    Created a decorator methpd for giving access to user to carryo out CRUD operations in notes
+
+    """
     def wrapper(request, *args, **kwargs):
         try:
-            token = request.META['HTTP_AUTHORIZATION']
-            decoded_token = jwt.decode(token, "secret", algorithms=["HS256"])
-            id = decoded_token['id']
-            user = User.objects.get(id = id)
-            if user and user.is_active:
+            token = request.META['HTTP_AUTHORIZATION'] #recieving the token from authorization header 
+            decoded_token = jwt.decode(token, "secret", algorithms=["HS256"]) #decoding the stringified token for id
+            id = decoded_token['id'] #Obtaining the id from the decoded token
+            user = User.objects.get(id = id) #matching with existing database id with the decoded id
+            if user and user.is_active: #if the id exists and user is active, it can carry out CRUD operations in notes
                 request.user = user
                 return view_func(request, *args, **kwargs)   
             response = {'success': False, 'message': 'User must be logged in'}
