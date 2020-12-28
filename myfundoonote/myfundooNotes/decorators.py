eaa4882d8cc4128f2298ac360351c247febf4e89
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-28 12:08:19 +0530

    [Ibraheem Khaleel]Refactored: code for decoding and checking token in redis to give access

--- myfundoonote/myfundooNotes/decorators.py
+++ myfundoonote/myfundooNotes/decorators.py
@@ -4,33 +4,48 @@ Created :  23 December 2020
 
 """
 
-import json
-import jwt
-
-from django.http import HttpResponse
-
 from rest_framework import status
-
+from django.http import HttpResponse
+import json
 from .models import User
+import jwt
+from services.cache import Cache
+from services.encrypt import Encrypt
+import logging
+
+
 
 
 def user_login_required(view_func): 
     """
-    Created a decorator methpd for giving access to user to carryo out CRUD operations in notes
+    Created a decorator methpd for giving access to user to carryo out CRUD operations in notes and labels
 
     """
     def wrapper(request, *args, **kwargs):
+        result = {'message': 'some other issue please try after some time', 'status': False}
         try:
-            token = request.META['HTTP_AUTHORIZATION'] #recieving the token from authorization header 
-            decoded_token = jwt.decode(token, "secret", algorithms=["HS256"]) #decoding the stringified token for id
-            id = decoded_token['id'] #Obtaining the id from the decoded token
-            user = User.objects.get(id = id) #matching with existing database id with the decoded id
-            if user and user.is_active: #if the id exists and user is active, it can carry out CRUD operations in notes
-                request.user = user
-                return view_func(request, *args, **kwargs)   
-            response = {'success': False, 'message': 'User must be logged in'}
-            return HttpResponse(json.dumps(response), status=status.HTTP_400_BAD_REQUEST)
-        except Exception:
-            response = {'success': False, 'message': 'please provide a valid token'}
-            return HttpResponse(json.dumps(response), status=status.HTTP_400_BAD_REQUEST)       
+            token = request.META.get('HTTP_AUTHORIZATION') #recieving the token from authorization header 
+            decoded_token = Encrypt.decode(token) #decoding the stringified token for id
+            if Cache.get_cache("TOKEN_"+str(decoded_token['id'])+"_AUTH") is not None: #Checking whether the token exists in redis
+                request.user = User.objects.get(id=decoded_token['id'])
+                result['message'] = 'token verification successful'
+                result['status'] = True
+                logging.debug('{} status_code = {}'.format(result, status.HTTP_200_OK))
+                return view_func(request, *args, **kwargs)
+            result['message'] = "logged in user's token is not provided"
+            logging.debug('{} status_code = {}'.format(result, status.HTTP_400_BAD_REQUEST))
+            return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
+        except jwt.ExpiredSignatureError as e:
+            result['message'] = 'Activation Expired'
+            logging.exception('{} exception = {}, status_code = {}'.format(result, str(e), status.HTTP_400_BAD_REQUEST))
+            return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
+        except jwt.exceptions.DecodeError as e:
+            result['message'] = 'Invalid Token'
+            logging.exception('{}, exception = {}, status_code = {}'.format(result, str(e), status.HTTP_400_BAD_REQUEST))
+            return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            result['message'] = 'some other issue please try after some time'
+            logging.exception('{}, exception = {}, status_code = {}'.format(result, str(e),  status.HTTP_400_BAD_REQUEST))
+            return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
     return wrapper
+           
