c77face16be40a68a8ddba805fec68fdfe13714a
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-28 12:10:53 +0530

    [Ibraheem Khaleel]Refactored: code for custom user model

--- myfundoonote/myfundooNotes/models.py
+++ myfundoonote/myfundooNotes/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.utils.translation import gettext_lazy as _
