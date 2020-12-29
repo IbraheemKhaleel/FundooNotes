3e65f06feae7343e43fd6fc91805221c8f24a761
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-29 11:39:04 +0530

    [Ibraheem Khaleel]Refactored: code for creating objects for cache class in decorators.

--- myfundoonote/myfundooNotes/decorators.py
+++ myfundoonote/myfundooNotes/decorators.py
@@ -14,8 +14,6 @@ from services.encrypt import Encrypt
 import logging
 
 
-
-
 def user_login_required(view_func): 
     """
     Created a decorator methpd for giving access to user to carryo out CRUD operations in notes and labels
@@ -25,14 +23,15 @@ def user_login_required(view_func):
         result = {'message': 'some other issue please try after some time', 'status': False}
         try:
             token = request.META.get('HTTP_AUTHORIZATION') #recieving the token from authorization header 
-            decoded_token = Encrypt.decode(token) #decoding the stringified token for id
-            if Cache.get_cache("TOKEN_"+str(decoded_token['id'])+"_AUTH") is not None: #Checking whether the token exists in redis
+            decoded_token = Encrypt.decode(token)
+            cache = Cache() #decoding the stringified token for id
+            if cache.get_cache("TOKEN_"+str(decoded_token['id'])+"_AUTH") is not None: #Checking whether the token exists in redis
                 request.user = User.objects.get(id=decoded_token['id'])
                 result['message'] = 'token verification successful'
                 result['status'] = True
                 logging.debug('{} status_code = {}'.format(result, status.HTTP_200_OK))
                 return view_func(request, *args, **kwargs)
-            result['message'] = "logged in user's token is not provided"
+            result['message'] = "logged in user's token is not provided" #change message to you need to login first
             logging.debug('{} status_code = {}'.format(result, status.HTTP_400_BAD_REQUEST))
             return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
         except jwt.ExpiredSignatureError as e:
