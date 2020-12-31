412a82235de1dcaa82fe2f7527015426fd975f42
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-12-31 10:13:02 +0530

    [Ibraheem Khaleel]Refactored: code for creating constructor for cache class

--- myfundoonote/services/cache.py
+++ myfundoonote/services/cache.py
@@ -1,24 +1,20 @@
 import redis
 from decouple import config
-       
-        
-#cache
+
+
+
+
 class Cache:
 
-    """
-    Created a class to set and get the respective token in redis cache
-    """
     def __init__(self):
-        self.r = redis.StrictRedis(host=config('REDIS_HOST'), port=config('REDIS_PORT'))   
+        self.r = redis.StrictRedis(host=config('REDIS_HOST'), port=config('REDIS_PORT'))
 
-    
-   
     def set_cache(self, key, value):
         """
-        it takes key and value value as inputs and stores it in redis server and has expiry time of 60 seconds
+        takes key[id] and value[token] value as inputs and stores it in redis server and has expiration time of 60 seconds
         """
         self.r.set(key, value)
-        self.r.expire(key, time=60)
+        self.r.expire(key, time=8000)
 
     
     def get_cache(self, key):
