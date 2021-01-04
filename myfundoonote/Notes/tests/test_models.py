4a5d8e42aa6e00e6ff1129463b8b3fdbfbff5b9e
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-04 12:32:56 +0530

    [Ibraheem Khaleel]Added: code for testing models of notes

---
+++ myfundoonote/Notes/tests/test_models.py
@@ -0,0 +1,8 @@
+import pytest
+from mixer.backend.django import mixer
+pytestmark = pytest.mark.django_db
+
+class TestmyfundooNotes:
+    def test_init(self):
+        note_obj = mixer.blend('Notes.Note')
+        assert note_obj.pk == 1, 'Should save an instance'
