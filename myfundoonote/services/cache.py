fb6781281ac5be39e38fa39545aee9200fd02b13
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-06 12:24:38 +0530

    [Ibraheem Khaleel]Refactored: code for deleting a prticular element from cache

--- myfundoonote/services/cache.py
+++ myfundoonote/services/cache.py
@@ -2,8 +2,6 @@ import redis
 from decouple import config
 
 
-
-
 class Cache:
 
     def __init__(self):
@@ -16,9 +14,16 @@ class Cache:
         self.r.set(key, value)
         self.r.expire(key, time=8000)
 
-    
     def get_cache(self, key):
         """
         it takes key as input and returns value stored with that key
         """
         return self.r.get(key)
+
+    def delete_cache(self, key):
+        """
+
+        @param key: The key of the respective note
+        @type key: String
+        """
+        self.r.delete(key)
