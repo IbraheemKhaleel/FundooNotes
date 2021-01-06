a275b307b08ed9f71ab4b16ab7a1cb2900366d30
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-06 19:42:32 +0530

    [Ibraheem Deleted: Unnecessary files have been removed

--- myfundoonote/Label/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('label/', views.LabelsView.as_view(), name = 'label_create_retrieve'),
-    path('label-update/<int:pk>/', views.LabelView.as_view(), name = 'label_put_delete')
-    ]
--- myfundoonote/Notes/tests/test_models.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from mixer.backend.django import mixer
-pytestmark = pytest.mark.django_db
-
-class TestmyfundooNotes:
-    def test_init(self):
-        note_obj = mixer.blend('Notes.Note')
-        assert note_obj.pk == 1, 'Should save an instance'
