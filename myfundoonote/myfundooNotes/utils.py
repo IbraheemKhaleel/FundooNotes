495c44bb05e2fa21b15cf59b4af50d4744a0932b
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-07 12:29:17 +0530

    [Ibraheem Khaleel]Refactored: code for sending emails to user

--- myfundoonote/myfundooNotes/utils.py
+++ myfundoonote/myfundooNotes/utils.py
@@ -1,6 +1,7 @@
 from django.core.mail import EmailMessage
 import threading
 
+
 class EmailThread(threading.Thread):
 
     def __init__(self, email):
@@ -10,10 +11,10 @@ class EmailThread(threading.Thread):
     def run(self):
         self.email.send()
 
+
 class Util:
     @staticmethod
     def send_email(data):
         email = EmailMessage(
-            subject=data['email_subject'],body=data['email_body'], to=[data['to_email']])
+            subject=data['email_subject'], body=data['email_body'], to=[data['to_email']])
         EmailThread(email).start()
-    
