caa0376c3e4b4edf9ff6c6d2647e5bab9f3a1f1d
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-08 17:53:21 +0530

    [Ibraheem Khaleel]Refactored: code for testing login,registration of user and label,note's CRUD operations

--- myfundoonote/myfundooNotes/tests/test_views.py
+++ myfundoonote/myfundooNotes/tests/test_views.py
@@ -1,6 +1,6 @@
-from django.test import RequestFactory
 from django.urls import reverse
 from mixer.backend.django import mixer
+from rest_framework.test import APIClient
 from myfundooNotes.views import Registration
 from rest_framework import status
 import pytest
@@ -11,41 +11,102 @@ User = get_user_model()
 
 
 @pytest.mark.django_db
-
 class Data(TestCase):
+    """
+    this class will initialise all the urls and data and it is inherited by other test classes
+    """
+
     def setUp(self):
-        self.register_url = reverse('register')
-        self.login_url = reverse('login')
+        """
+        this method setup all the url and data which was required by all test cases
+        """
+        self.register_url = reverse("register")
+        self.login_url = reverse("login")
+        self.label_post_url = reverse("manage-labels")
+        self.label_url = reverse("manage-label", kwargs={'pk': 1})
+        self.note_post_url = reverse("manage-notes")
+        self.note_url = reverse("manage-specific-notes", kwargs={'pk': 1})
+        self.note_archived_url = reverse("archived-notes")
+        self.single_note_archived_url = reverse("manage-specific-archived", kwargs={'pk': 1})
+        self.note_pinned_url = reverse("pinned-notes")
+        self.single_note_pinned_url = reverse("manage-specific-pinned-notes", kwargs={'pk': 4})
+        self.note_trash_url = reverse("trashed-notes")
+        self.single_note_trash_url = reverse("manage-specific-trashed-notes", kwargs={'pk': 1})
+        self.note_search_url = reverse("search-notes")+"?search_term="+ "valid"
+
         self.valid_registration_data = {'first_name': "Ibraheem",
                                         'last_name': "Khaleel",
-                                        'email': "ibrah@gmail.com",
-                                        'user_name': "ibrahkhaleel",
-                                        'password': "itisnotmypassport"}
-        self.invalid_registration_data = {'first_name': "12",
-                                          'last_name': "56",
-                                          'email': "abc1",
-                                          'user_name': "abc"}
-        self.valid_login_data = {
-            'email': "ibrah@gmail.com",
-            'password': "itisnotmypassport"}
-        self.invalid_login_data = {
-            'email': "ibrah@gmail.com",
-            'password': "itis"}
+                                        'email': "IbraheemKhaleel96@gmail.com",
+                                        'user_name': "IbraheemKhaleel",
+                                        'password': "qwerty12"}
 
+        self.invalid_registration_data = {'first_name': "abc",
+                                          'last_name': "def",
+                                          'email': "abc123@gmail.com",
+                                          'user_name': "abcdef"}
+        self.valid_login_data = {
+            'email': "IbraheemKhaleel96@gmail.com",
+            'password': "qwerty12"}
+        self.invalid_login_data = {
+            'email': "123abc@gmail.com",
+            'password': "abcdef"}
+
+        self.valid_label_data = {
+            'name': "label note",
+        }
+
+        self.valid_label_put_data = {'name': "Test Note",
+                                     }
+        self.invalid_label_data = {'labelname': "wrong note",
+                                   }
+
+        self.valid_note_put_data = {
+            "title": "randomnote",
+            "description": "random description",
+            "is_archived": True,
+            "is_pinned": True,
+            "labels": ["label note"],
+            "collaborators": ["IbraheemKhaleel96@gmail.com"]
+        }
+        self.invalid_note_data = {
+            'title': "note title",
+            'description': "this is my description",
+            'labels': "Qwerty Note",
+            'collaborators': ["abc123@gmail.com"]
+        }
+
+        self.valid_note_data = {
+            "title": "valid note",
+            "description": "this is my test note",
+            "is_archived": True,
+            "is_pinned": True,
+            "labels": ["label note"],
+            "collaborators": ["IbraheemKhaleel96@gmail.com"]
+        }
+        self.valid_note_data2 = {
+            "title": "valid note 2",
+            "description": "this is my 2nd test note",
+            "is_trashed": True,
+            "labels": ["label note"],
+            "collaborators": ["IbraheemKhaleel96@gmail.com"]
+        }
 
 class RegistrationTests(Data):
+    """
+    this class will test registration view and match with status_code
+    """
 
-    def test_given_valid_details(self):
+    def test_given_valid_details_for_registration(self):
         """
-        Ensure we can create a new account object.
+        Ensure we can create a new account object and it returns status code as 201.
         """
 
         response = self.client.post(self.register_url, self.valid_registration_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_given_invalid_details(self):
+    def test_given_invalid_details_for_registration(self):
         """
-        Ensure we cannot create a new account object.
+        Ensure we cannot create a new account object and returns status code as 400.
         """
 
         response = self.client.post(self.register_url, self.invalid_registration_data, format='json')
@@ -56,20 +117,19 @@ class LoginTest(Data):
 
     def test_given_valid_credentials_login(self):
         """
-        Ensure we can login.
+        Ensure we can login and return status code as 200.
         """
 
         self.client.post(self.register_url, self.valid_registration_data, format='json')
         user = User.objects.filter(email=self.valid_registration_data['email']).first()
         user.is_active = True
         user.save()
-
         response = self.client.post(self.login_url, self.valid_login_data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_given_invalid_credentials_for_login(self):
         """
-        Ensure we cannot login.
+        Ensure we cannot login and returns status code as 400.
         """
 
         self.client.post(self.register_url, self.valid_registration_data, format='json')
@@ -78,4 +138,200 @@ class LoginTest(Data):
         user.save()
 
         response = self.client.post(self.login_url, self.invalid_login_data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        assert response.status_code == 400
+
+
+class LabelTest(Data):
+
+    def test_given_valid_label_details_for_crud(self):
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+
+        response = self.client.post(self.label_post_url, self.valid_label_data, HTTP_AUTHORIZATION=headers,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = self.client.get(self.label_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.delete(self.label_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_given_invalid_label_details_for_crud(self):
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+        response = self.client.post(self.label_post_url, self.invalid_label_data, HTTP_AUTHORIZATION=headers,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.get(self.label_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        response = self.client.put(self.label_url, self.valid_label_put_data, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        response = self.client.delete(self.label_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class NotesTest(Data):
+    """
+    Test case for validating Notes class with valid and invalid details.
+    """
+
+    def test_notes_with_valid_details(self):
+        """
+        Test case for validating Labels class with valid details.
+        """
+        client = APIClient()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+
+        response = self.client.post(self.label_url, self.valid_label_data, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        response = client.post(self.note_post_url, self.valid_note_data, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        response = client.put(self.note_url, self.valid_note_put_data, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get(self.note_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.delete(self.note_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_notes_with_invalid_details(self):
+        """
+        Test case for validating Labels class with invalid details.
+        """
+        client = APIClient()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+
+        response = self.client.post(self.note_post_url, self.invalid_note_data, HTTP_AUTHORIZATION=headers,
+                                    format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.get(self.note_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+        response = self.client.put(self.note_url, self.valid_note_put_data, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response = self.client.delete(self.note_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class ArchivedViewTest(Data):
+    """
+    Test case for validating ArchivedViewTest class with valid and invalid details.
+    """
+
+    def test_archived_view_for_valid_details(self):
+        """
+        Test case for validating ArchivedView class with valid details.
+        """
+        client = APIClient()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+        self.client.post(self.label_url, self.valid_label_data, HTTP_AUTHORIZATION=headers, format='json')
+        client.post(self.note_post_url, self.valid_note_data, HTTP_AUTHORIZATION=headers, format='json')
+        client.post(self.note_post_url, self.valid_note_data2, HTTP_AUTHORIZATION=headers, format='json')
+
+        response = client.get(self.note_archived_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = client.get(self.single_note_archived_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class PinnedViewTest(Data):
+    """
+    Test case for validating PinnedView class with valid and invalid details.
+    """
+
+    def test_pinned_view_for_valid_details(self):
+        """
+        Test case for validating PinnedView class with valid details.
+        """
+        client = APIClient()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+        self.client.post(self.label_url, self.valid_label_data, HTTP_AUTHORIZATION=headers, format='json')
+        client.post(self.note_post_url, self.valid_note_data, HTTP_AUTHORIZATION=headers, format='json')
+        client.post(self.note_post_url, self.valid_note_data2, HTTP_AUTHORIZATION=headers, format='json')
+
+        response = client.get(self.note_pinned_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # response = client.get(self.single_note_pinned_url, HTTP_AUTHORIZATION=headers, format='json')
+        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TrashViewTest(Data):
+
+    def test_trash_view_for_valid_details(self):
+        client = APIClient()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+        self.client.post(self.label_url, self.valid_label_data, HTTP_AUTHORIZATION=headers, format='json')
+        client.post(self.note_post_url, self.valid_note_data2, HTTP_AUTHORIZATION=headers, format='json')
+
+        response = self.client.get(self.note_trash_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response = self.client.get(self.single_note_trash_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class SearchViewTest(Data):
+
+    def test_search_view_for_valid_details(self):
+        client = APIClient()
+        self.client.post(self.register_url, self.valid_registration_data, format='json')
+        user = User.objects.filter(email=self.valid_registration_data['email']).first()
+        user.is_verified = True
+        user.is_active = True
+        user.save()
+        response = self.client.post(self.login_url, self.valid_login_data, format='json')
+        headers = response.data['data']
+        self.client.post(self.label_url, self.valid_label_data, HTTP_AUTHORIZATION=headers, format='json')
+        client.post(self.note_post_url, self.valid_note_data, HTTP_AUTHORIZATION=headers, format='json')
+        response = self.client.get(self.note_search_url, HTTP_AUTHORIZATION=headers, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
