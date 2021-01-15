e42f6824a7bcb12441d9c5f29c32d832bce5478b
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-16 00:00:29 +0530

    [Ibraheem Khaleel]Refactored: code for serializing and deserializing the data

--- myfundoonote/Notes/serializers.py
+++ myfundoonote/Notes/serializers.py
@@ -1,13 +1,8 @@
 from rest_framework import serializers
 from .models import Note
-from myfundooNotes.models import User
-from rest_framework.response import Response
-from Label.models import Label
+
 
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
         fields = '__all__'
-
-    
-   
