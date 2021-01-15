d7ce6eca18d98f0c179266dd42b57a5045923783
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-16 00:05:33 +0530

    [Ibraheem Khaleel]Refactored: code for  urls for labels

--- myfundoonote/labels/urls.py
+++ myfundoonote/labels/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('manage-labels/', views.ManageLabel.as_view(), name='manage-labels'),
-    path('manage-labels/<int:pk>/', views.ManageLabel.as_view(), name='manage-label')
+    path('manage-labels/<int:pk>/', views.ManageLabel.as_view(), name='manage-label'),
 ]
