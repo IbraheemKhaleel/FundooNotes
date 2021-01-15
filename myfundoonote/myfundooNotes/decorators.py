ec8a6d38215096c1fe1741b909d80bcc6499d1ed
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-15 23:56:29 +0530

    [Ibraheem Khaleel]Refactored: code for allowing the respected user to access views of notes and labels using token

--- myfundoonote/myfundooNotes/decorators.py
+++ myfundoonote/myfundooNotes/decorators.py
@@ -7,12 +7,10 @@ Created :  23 December 2020
 from rest_framework import status
 from django.http import HttpResponse
 import json
-from .models import User
 import jwt
 from services.cache import Cache
 from services.encrypt import Encrypt
 import logging
-
 
 
 def user_login_required(view_func):
@@ -22,32 +20,29 @@ def user_login_required(view_func):
     Args:
         view_func ([request]): [the get,post etc view requested]
     """
+
     def wrapper(request, *args, **kwargs):
         result = {}
         try:
             token = request.META['HTTP_AUTHORIZATION']
             decoded_token = Encrypt.decode(token)
-            print(decoded_token)
             cache = Cache()
-            if cache.get_cache("TOKEN_"+str(decoded_token['id'])+"_AUTH") is not None:
-                print("hi")
+            if cache.get_cache("TOKEN_" + str(decoded_token['id']) + "_AUTH") is not None:
                 kwargs['userid'] = decoded_token['id']
-                print(kwargs['userid'])
-                return view_func(request, *args , **kwargs)   
-            return HttpResponse('UnauThorized',status.HTTP_403_FORBIDDEN)
-            #result = utils.manage_response(status=False,message='User must be logged in')
-            #return HttpResponse(json.dumps(result), status=status.HTTP_400_BAD_REQUEST)
+                return view_func(request, *args, **kwargs)
+            return HttpResponse('Unauthorized access is detected', status.HTTP_403_FORBIDDEN)
         except jwt.ExpiredSignatureError as e:
             result['message'] = 'Activation Expired'
             logging.exception('{} exception = {}, status_code = {}'.format(result, str(e), status.HTTP_400_BAD_REQUEST))
             return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
         except jwt.exceptions.DecodeError as e:
             result['message'] = 'Invalid Token'
-            logging.exception('{}, exception = {}, status_code = {}'.format(result, str(e), status.HTTP_400_BAD_REQUEST))
+            logging.exception(
+                '{}, exception = {}, status_code = {}'.format(result, str(e), status.HTTP_400_BAD_REQUEST))
             return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             result['message'] = 'some other issue please try after some time'
-            logging.exception('{}, exception = {}, status_code = {}'.format(result, str(e),  status.HTTP_400_BAD_REQUEST))
+            logging.exception(
+                '{}, exception = {}, status_code = {}'.format(result, str(e), status.HTTP_400_BAD_REQUEST))
             return HttpResponse(json.dumps(result), status.HTTP_400_BAD_REQUEST)
     return wrapper
-           
