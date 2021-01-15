fe1c82bfcd325a1c21cd99e02d5043bc2945fc12
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-16 00:02:27 +0530

    [Ibraheem Khaleel]Refactored: code for sending reminder email to the user using celery beat and rabbitmq

--- myfundoonote/myfundooNotes/tasks.py
+++ myfundoonote/myfundooNotes/tasks.py
@@ -4,6 +4,8 @@ import logging
 import os
 import datetime
 
+from myfundooNotes.models import User
+from myfundoonote.settings import MINUTES_TO_SECOND, HOUR_TO_MINUTE, SECONDS_IN_A_DAY
 import pytz
 from django.core.mail import EmailMessage
 import threading
@@ -55,24 +57,17 @@ def check_reminder():
     """
     Reminds the user if any reminder set in their notes before an hour
     """
-    print('something')
     current_time = datetime.datetime.now()
-    print('something more')
     utc = pytz.UTC
-    #print(utc)
     current_time = utc.localize(current_time)
-    #print(current_times.tzinfo)
-    #print(current_times)
-    for note in Note.objects.filter(reminder__gt=current_time).exclude(reminder=None):
-        #print('not in loop')
+    logger.debug(current_time)
+    for note in Note.objects.exclude(reminder=None).filter(reminder__gt=current_time): #Checking whether any reminder is present in any note objects
+        user = User.objects.get(id=note.user_id)
         difference = note.reminder - current_time
-        seconds_in_day = 24 * 60 * 60
-        minutes_remaining = (divmod(difference.days * seconds_in_day + difference.seconds, 60))
-        if minutes_remaining[0] < 60:
-            data = {'email_body': 'Hi!Reminder for ' + note.title + ' is scheduled within this hour.',
-                    'to_email': 'ikhaleelem@gmail.com',
+        minutes_remaining = (divmod(difference.days * SECONDS_IN_A_DAY + difference.seconds, MINUTES_TO_SECOND))
+        if minutes_remaining[0] < HOUR_TO_MINUTE:
+            data = {'email_body': 'This is a reminder' + note.title + ' is scheduled in an hour.',
+                    'to_email': user.email,
                     'email_subject': 'Reminder for your note'}
             send_email.delay(data)
-            return 'done'  # sends reminder email to the user
-        #return 'note not found'
-    return 'not a reminder'
+            logger.debug('sent reminder for ' + note.title)
