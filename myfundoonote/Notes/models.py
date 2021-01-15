4dc3a31fc60ad8a009dc9b90893f778c88bc7cc2
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-15 23:57:58 +0530

    [Ibraheem Khaleel]Refactored: code for including trashed and soft delete the notes

--- myfundoonote/Notes/models.py
+++ myfundoonote/Notes/models.py
@@ -2,7 +2,7 @@ from colorfield.fields import ColorField
 from django.db import models
 from django.utils import timezone
 from myfundooNotes.models import User
-from Label.models import Label
+from labels.models import Label
 
 
 class Note(models.Model):
@@ -10,20 +10,22 @@ class Note(models.Model):
     Created a model class for Notes to store respective fields
     
     """
-    user = models.ForeignKey(User , on_delete = models.CASCADE , related_name = 'author', null = True) #name of user
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author', null=True)  # name of user
     deleted_at = models.DateTimeField(blank=True, null=True)
-    title = models.CharField(max_length = 50, blank = True)
-    description = models.TextField(blank = True)
-    reminder = models.DateTimeField(null = True, blank = True)
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    reminder = models.DateTimeField(null=True, blank=True)
     color = ColorField(default='#FFFFFF')
-    image = models.ImageField(upload_to = 'notes/', null = True,blank = True)
-    collaborators = models.ManyToManyField(User , related_name = 'collaborator',  blank = True)#It connects with User model class we defined in myfundooNotes file
-    labels = models.ManyToManyField(Label, blank= True) #This connects Label model class we defined above 
-    is_archived = models.BooleanField(default = False, blank = True)
-    is_deleted = models.BooleanField(default = False)
-    is_pinned = models.BooleanField(default = False, blank = True, null = True)
+    image = models.ImageField(upload_to='notes/', null=True, blank=True)
+    collaborators = models.ManyToManyField(User, related_name='collaborator',
+                                           blank=True)  # It connects with User model class we defined in myfundooNotes file
+    labels = models.ManyToManyField(Label, blank=True)  # This connects Label model class we defined above
+    is_archived = models.BooleanField(default=False, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    is_pinned = models.BooleanField(default=False, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now = True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_trashed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
@@ -31,6 +33,8 @@ class Note(models.Model):
     def soft_delete(self):
         self.is_deleted = True
         self.deleted_at = timezone.now()
-        self.save()    
+        self.save()
 
-    
+    def trashed(self):
+        self.is_trashed = True
+        self.save()
