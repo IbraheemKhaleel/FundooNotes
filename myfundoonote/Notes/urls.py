8658ac55a25daf7ce9a4195027c69e74b0303e01
IbraheemKhaleel <ikhaleelem@gmail.com>
2021-01-18 13:27:31 +0530

    [Ibraheem Khaleel]Refactored: code for  urls for Notes

--- myfundoonote/Notes/urls.py
+++ myfundoonote/Notes/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.NotesOverview.as_view()),
-    path('manage-notes/archived-notes/', views.ManageArchivedNote.as_view(), name='archived-notes'),
-    path('manage-notes/archived-notes/<int:pk>/', views.ManageArchivedNote.as_view(), name='manage-specific-archived'),
-    path('manage-notes/trashed-notes/<int:pk>/',views.ManageTrashedNotes.as_view(),name ='manage-specific-trashed-notes'),
-    path('manage-notes/trashed-notes/',views.ManageTrashedNotes.as_view(),name = 'trashed-notes'),
-    path('manage-notes/pinned-notes/<int:pk>/',views.ManagePinnedNotes.as_view(),name = 'manage-specific-pinned-notes'),
-    path('manage-notes/pinned-notes/',views.ManagePinnedNotes.as_view(),name = 'pinned-notes'),
-    path('manage-notes/', views.ManageNotes.as_view(), name='manage-notes'),
-    path('manage-notes/<int:pk>/', views.ManageNotes.as_view(), name='manage-specific-notes'),
-    path('manage-notes/search-notes/', views.SearchNote.as_view(), name='search-notes')
+    path('notes/archived-notes/', views.ManageArchivedNote.as_view(), name='archived-notes'),
+    path('note/archived-note/<int:pk>/', views.ManageArchivedNote.as_view(), name='manage-specific-archived'),
+    path('note/trashed-note/<int:pk>/',views.ManageTrashedNotes.as_view(),name ='manage-specific-trashed-notes'),
+    path('notes/trashed-notes/',views.ManageTrashedNotes.as_view(),name = 'trashed-notes'),
+    path('note/pinned-note/<int:pk>/',views.ManagePinnedNotes.as_view(),name = 'manage-specific-pinned-notes'),
+    path('notes/pinned-notes/',views.ManagePinnedNotes.as_view(),name = 'pinned-notes'),
+    path('notes/', views.ManageNotes.as_view(), name='manage-notes'),
+    path('note/<int:pk>/', views.ManageNotes.as_view(), name='manage-specific-notes'),
+    path('notes/search-notes/', views.SearchNote.as_view(), name='search-notes')
 ]
